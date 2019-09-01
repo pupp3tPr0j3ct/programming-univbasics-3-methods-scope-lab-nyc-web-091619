009f1f7606e9e143a1940e199f568b1e9ff9ac88
Jason Santana <jsantana006@gmail.com>
2019-09-01 21:49:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,11 @@
+phrase = "It's a me, Mario!"
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
   puts status
 end
+
